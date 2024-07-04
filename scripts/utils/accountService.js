8930fef7ac47d20302a39ceec10d0a46b1dedba5
Heber Futuri <h@futuri.dev>
2024-07-03 20:17:13 -0500

    -

--- scripts/utils/accountService.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-export const accountService = {
-  getAccount: () => axios.get('/supporters/api/v1/users/me.json'),
-}
--- scripts/utils/newsletterService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const apiUrl = '/contact.js'
-
-export const newsletterService = {
-  register: (formData) => axios.post(apiUrl, formData),
-}
